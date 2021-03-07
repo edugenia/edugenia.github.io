013e8dd8ec76ef15e8368cfbc36ab74be9bd8af0
Mario Càllisto <mario.callisto@gmail.com>
2021-03-07 21:51:00 +0100

    New website with demo smartphone app

---
+++ 50e08c99.c1f5293b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{77:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"edugenia","name":"edugenia","count":1,"permalink":"/blog/tags/edugenia"}')}}]);
